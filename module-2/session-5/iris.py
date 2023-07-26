8373807a81375ef719a5e7c697bf552780cb6e25
carloslme <32799711+carloslme@users.noreply.github.com>
2023-07-25 18:28:26 -0600

    add iris file

---
+++ module-2/session-5/iris.py
@@ -0,0 +1,32 @@
+from sklearn.datasets import load_iris
+
+import numpy as np
+
+from sklearn.linear_model import LogisticRegression
+
+# Load data from sklearn
+X, y = load_iris(return_X_y=True)
+
+# Train the model using regresion logistic
+clf = LogisticRegression(solver='lbfgs',
+                         max_iter=1000,
+                         multi_class='multinomial').fit(X, y)
+# Define iris types
+iris_type = {
+    0: 'setosa',
+    1: 'versicolor',
+    2: 'virginica'
+}
+
+
+# Define dummy values
+sepal_length, sepal_width, petal_length, petal_width = 2, 3, 4, 6
+
+X = [sepal_length, sepal_width, petal_length, petal_width]
+
+
+# Make a prediction
+
+prediction = clf.predict_proba([X])
+print({'class': iris_type[np.argmax(prediction)],
+      'probability': round(max(prediction[0]), 2)})
